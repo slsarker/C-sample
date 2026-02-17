703783ee34cb05ad5e74098abd59d3a0ded287db
SAGOR <sls.aiub@gmail.com>
2026-02-17 09:13:25 +0600

    c sharp project

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace SimpleCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonSum = new System.Windows.Forms.Button();
+            this.buttonSub = new System.Windows.Forms.Button();
+            this.buttonMul = new System.Windows.Forms.Button();
+            this.buttonDib = new System.Windows.Forms.Button();
+            this.buttonClr = new System.Windows.Forms.Button();
+            this.textBoxAns = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(238, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Simple Calculator";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(97, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter First Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(97, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Second Number:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(353, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(353, 230);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(210, 30);
+            this.textBox2.TabIndex = 4;
+            // 
+            // buttonSum
+            // 
+            this.buttonSum.BackColor = System.Drawing.Color.Aqua;
+            this.buttonSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSum.Location = new System.Drawing.Point(102, 315);
+            this.buttonSum.Name = "buttonSum";
+            this.buttonSum.Size = new System.Drawing.Size(63, 35);
+            this.buttonSum.TabIndex = 5;
+            this.buttonSum.Text = "+";
+            this.buttonSum.UseVisualStyleBackColor = false;
+            this.buttonSum.Click += new System.EventHandler(this.buttonSum_Click);
+            // 
+            // buttonSub
+            // 
+            this.buttonSub.BackColor = System.Drawing.Color.Aqua;
+            this.buttonSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSub.Location = new System.Drawing.Point(187, 315);
+            this.buttonSub.Name = "buttonSub";
+            this.buttonSub.Size = new System.Drawing.Size(63, 35);
+            this.buttonSub.TabIndex = 6;
+            this.buttonSub.Text = "-";
+            this.buttonSub.UseVisualStyleBackColor = false;
+            this.buttonSub.Click += new System.EventHandler(this.buttonSub_Click);
+            // 
+            // buttonMul
+            // 
+            this.buttonMul.BackColor = System.Drawing.Color.Aqua;
+            this.buttonMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMul.Location = new System.Drawing.Point(279, 315);
+            this.buttonMul.Name = "buttonMul";
+            this.buttonMul.Size = new System.Drawing.Size(63, 35);
+            this.buttonMul.TabIndex = 7;
+            this.buttonMul.Text = "x";
+            this.buttonMul.UseVisualStyleBackColor = false;
+            this.buttonMul.Click += new System.EventHandler(this.buttonMul_Click);
+            // 
+            // buttonDib
+            // 
+            this.buttonDib.BackColor = System.Drawing.Color.Aqua;
+            this.buttonDib.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDib.Location = new System.Drawing.Point(374, 315);
+            this.buttonDib.Name = "buttonDib";
+            this.buttonDib.Size = new System.Drawing.Size(63, 35);
+            this.buttonDib.TabIndex = 8;
+            this.buttonDib.Text = "/";
+            this.buttonDib.UseVisualStyleBackColor = false;
+            this.buttonDib.Click += new System.EventHandler(this.buttonDib_Click);
+            // 
+            // buttonClr
+            // 
+            this.buttonClr.BackColor = System.Drawing.Color.Red;
+            this.buttonClr.FlatAppearance.BorderSize = 0;
+            this.buttonClr.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClr.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonClr.Location = new System.Drawing.Point(425, 392);
+            this.buttonClr.Name = "buttonClr";
+            this.buttonClr.Size = new System.Drawing.Size(78, 46);
+            this.buttonClr.TabIndex = 10;
+            this.buttonClr.Text = "Clr";
+            this.buttonClr.UseVisualStyleBackColor = false;
+            this.buttonClr.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBoxAns
+            // 
+            this.textBoxAns.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.textBoxAns.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAns.Location = new System.Drawing.Point(510, 315);
+            this.textBoxAns.Name = "textBoxAns";
+            this.textBoxAns.Size = new System.Drawing.Size(262, 56);
+            this.textBoxAns.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(815, 580);
+            this.Controls.Add(this.textBoxAns);
+            this.Controls.Add(this.buttonClr);
+            this.Controls.Add(this.buttonDib);
+            this.Controls.Add(this.buttonMul);
+            this.Controls.Add(this.buttonSub);
+            this.Controls.Add(this.buttonSum);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SimpleCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonSum;
+        private System.Windows.Forms.Button buttonSub;
+        private System.Windows.Forms.Button buttonMul;
+        private System.Windows.Forms.Button buttonDib;
+        private System.Windows.Forms.Button buttonClr;
+        private System.Windows.Forms.TextBox textBoxAns;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBoxAns.Clear();
+
+            textBox1.Focus();
+        }
+
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+
+            int sum = num1 + num2;
+            textBoxAns.Text = sum.ToString();
+        }
+
+        private void buttonSub_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+
+            int sub = num1-num2;
+            textBoxAns.Text = sub.ToString();
+        }
+
+        private void buttonMul_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+
+            long mul = num1 * num2;
+            textBoxAns.Text = mul.ToString();
+        }
+
+        private void buttonDib_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+
+            if (num2 == 0)
+            {
+                MessageBox.Show("Any number can not divided by 0");
+            }
+            else
+            {
+                double div = num1 / num2;
+                textBoxAns.Text = div.ToString();
+            }
+        }
+    }
+}
